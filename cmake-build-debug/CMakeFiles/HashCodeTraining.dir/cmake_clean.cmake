3e02e84922cf0a6891837089f6e998e4afbfb1ce
Bernadzikovskiy Slavik <vbernadzikovskiy@gmail.com>
2020-02-02 22:47:47 +0200

    init commit

---
+++ cmake-build-debug/CMakeFiles/HashCodeTraining.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HashCodeTraining.dir/main.cpp.o"
+  "HashCodeTraining.pdb"
+  "HashCodeTraining"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HashCodeTraining.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
