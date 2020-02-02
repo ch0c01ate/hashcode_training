9740f55f9d993929077485f0b48c68a4a10d1247
Bernadzikovskiy Slavik <vbernadzikovskiy@gmail.com>
2020-02-02 23:15:40 +0200

    added reading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string filename;
+    // EXAMPLE: ../data/a_example.in
+    cin >> filename;
+    ifstream fs(filename);
+    int max_number, pizza_number;
+    fs >> max_number;
+    fs >> pizza_number;
+    int pizza_sizes[pizza_number];
+    cout << max_number << endl << pizza_number << endl;
+    for(int i = 0; i < pizza_number; i++) fs >> pizza_sizes[i];
+    for(int i = 0; i < pizza_number; i++) cout << pizza_sizes[i];
     return 0;
 }
