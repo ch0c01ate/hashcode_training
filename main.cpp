ab3ce1e668668d730e52bfd320d2d9b6a1ea5998
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-02-03 21:23:33 +0200

    Added formatting

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+
 using namespace std;
 
 
-
-vector<string> split(string filename, char delim){
+vector<string> split(string filename, char delim) {
     vector<string> tokens;
     size_t prev = 0, pos = 0;
-    do
-    {
+    do {
         pos = filename.find(delim, prev);
         if (pos == string::npos) pos = filename.length();
-        string token = filename.substr(prev, pos-prev);
+        string token = filename.substr(prev, pos - prev);
         if (!token.empty()) tokens.push_back(token);
         prev = pos + 1;
-    }
-    while (pos < filename.length() && prev < filename.length());
+    } while (pos < filename.length() && prev < filename.length());
     return tokens;
 }
 
@@ -34,12 +32,10 @@ int main() {
     fs >> max_number;
     fs >> pizza_number;
     int pizza_sizes[pizza_number];
-    // cout << max_number << endl << pizza_number << endl;
-    for (int i = 0; i < pizza_number; i++) fs >> pizza_sizes[i];
-    // for(int i = 0; i < pizza_number; i++) cout << pizza_sizes[i];
+    for (int i = 0; i < pizza_number; i++) {
+        fs >> pizza_sizes[i];
+    }
     int M[pizza_number + 1][max_number + 1];
-    //int *M = new int[()*(max_number + 1)];
-    //std::vector<std::vector<int> > M(pizza_number + 1, std::vector<int>(max_number + 1));
     for (int w = 0; w <= max_number; w++) M[0][w] = 0;
     for (int i = 1; i <= pizza_number; i++) {
         for (int w = 0; w <= max_number; w++) {
@@ -50,22 +46,24 @@ int main() {
             }
         }
     }
-    int number = pizza_number, current_max = max_number, solution[pizza_number], i = 0;
+    int number = pizza_number;
+    int current_max = max_number;
+    int solution[pizza_number];
+    int i = 0;
 
-    while(number > 0) {
-        if(pizza_sizes[number-1] + M[number-1][current_max - pizza_sizes[number-1]] == M[number][current_max]){
-            current_max -= pizza_sizes[number-1];
-            solution[i] = number-1;
+    while (number > 0) {
+        if (pizza_sizes[number - 1] + M[number - 1][current_max - pizza_sizes[number - 1]] == M[number][current_max]) {
+            current_max -= pizza_sizes[number - 1];
+            solution[i] = number - 1;
             i++;
         }
         number--;
     }
-
     ofstream result;
     string folder_name = "../results/";
     result.open(folder_name.append(letter).append("_result.txt"));
     result << i << endl;
-    for(int j = i-1; j>0; j--) result << solution[j] << " ";
+    for (int j = i - 1; j > 0; j--) result << solution[j] << " ";
     result << solution[0] << endl;
     result.close();
 
